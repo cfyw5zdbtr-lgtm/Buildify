187304a57a78aefc2e40ae3b4798ce19893a1c1a
cfyw5zdbtr-lgtm <cfyw5zdbtr@privaterelay.appleid.com>
2026-07-04 22:13:30 +1200

    Add files via upload

---
+++ lib/constants.ts
@@ -0,0 +1,28 @@
+export const NAV_LINKS = [
+  { href: "/how-it-works", label: "How It Works" },
+  { href: "/new-builds", label: "Find New Builds" },
+  { href: "/investors", label: "For Investors" },
+  { href: "/about", label: "About" },
+  { href: "/resources", label: "Resources" },
+];
+
+export const FOOTER_LINKS = [
+  { href: "/about", label: "About Buildify" },
+  { href: "/how-it-works", label: "How It Works" },
+  { href: "/join-the-team", label: "Join the Team" },
+  { href: "/resources", label: "Resources" },
+  { href: "/contact", label: "Contact" },
+];
+
+export const LEGAL_LINKS = [
+  { href: "/privacy", label: "Privacy Policy" },
+  { href: "/terms", label: "Terms of Use" },
+];
+
+export const REGION_LINKS = [
+  { href: "/new-builds/waikato", label: "Waikato" },
+  { href: "/new-builds/waikato/hamilton", label: "Hamilton" },
+  { href: "/new-builds/waikato/cambridge", label: "Cambridge" },
+  { href: "/new-builds/waikato/te-awamutu", label: "Te Awamutu" },
+  { href: "/new-builds/auckland", label: "Auckland" },
+];
